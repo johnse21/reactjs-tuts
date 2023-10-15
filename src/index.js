380e83c0a88956f7e70a09835512b3b7937a04b9
johnse21 <john21aparejado@gmail.com>
2023-10-15 18:01:06 +0800

    videos til "Key props and spread operator"

--- src/index.js
+++ src/index.js
@@ -4,24 +4,34 @@ import './index.css';
 
 const books = [
   {
+    id: '162asdc211',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81BFQlGo5XL._AC_UL127_SR127,127_.jpg',
     author: 'John Doe',
     title: 'How to Catch a Witch'
   }, {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL254_SR254,254_.jpg',
+    id: '123ssggh14',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL127_SR127,127_.jpg',
     author: 'Myra Bell',
     title: 'Lessons in Chemistry: A Novel'
+  }, {
+    id: 'axv2123545',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71pL+3nMzfL._AC_UL127_SR127,127_.jpg',
+    author: 'Evandre Luke',
+    title: 'No Brainer (Diary of a Wimpy Kid Book 18)'
   }
-]
+];
 
 function BookList(){
   return (
     <section className='booklist'>
-      <Book 
-        img = {firstBook.img}
-        author = {firstBook.author}
-        title = {firstBook.title}
-      />
+      {books.map((book) => {
+        return (
+          <Book 
+            key = {book.id} 
+            {...book}
+          />
+        )
+      })}
     </section>
   )
 }
